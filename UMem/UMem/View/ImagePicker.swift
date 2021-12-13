152962011cdbf13b0eb82d87a69d305cd371c5f2
Qiao Liang <57929780+redifinition@users.noreply.github.com>
2021-12-13 16:38:19 +0800

    完成照片库的选取和自带相机拍摄功能，照片列表得以展示

---
+++ UMem/UMem/View/ImagePicker.swift
@@ -0,0 +1,63 @@
+//
+//  ImagePicker.swift
+//  UMem
+//
+//  Created by lq on 2021/12/13.
+//
+
+import Foundation
+
+
+import UIKit
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    @Binding var selectedImageList: [UIImage]
+    @Environment(\.presentationMode) private var presentationMode
+    
+    @EnvironmentObject var model:PhotoCapturerViewModel
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        
+        
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let photo = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImageList.append(photo)
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+
--- UMem/UMem/View/PictureResultView.swift
+++ UMem/UMem/View/PictureResultView.swift
@@ -12,12 +12,19 @@ struct PictureResultView: View {
     //视频捕捉的viewModel
     @EnvironmentObject  var model : PhotoCapturerViewModel
     
-    @State private var image: Image?
-    @State private var shouldPresentImagePicker = false
-    @State private var shouldPresentActionScheet = false
-    @State private var shouldPresentCamera = false
     
-    private var ColumnGrid = [GridItem(.flexible()),GridItem(.flexible())]
+    @State private var imageListOfLibrary :[UIImage] = []
+
+    
+    //照片库选择框
+    @State var isShowChoosingSheet = false
+    
+    //使用照片库还是自带相机
+    @State var capturingType = UIImagePickerController.SourceType.camera
+    
+    @State var isShowPhotoLibray = false
+    
+    private var ColumnGrid = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
     
 
     
@@ -25,49 +32,62 @@ struct PictureResultView: View {
             VStack{
                 ScrollView(.vertical){
                     LazyVGrid(columns: ColumnGrid, spacing:15){
+                        Button(action: {
+                            //首先结束相机捕捉
+                            self.model.stopCapturing()
+                            //sheet开启
+                            self.isShowChoosingSheet = true
+                            
+                            
+                        }, label: {
+                            Image("AdditionPhoto")
+                                .resizable()
+                                .frame(width: 85,height: 85)
+                        }).shadow(color:Color(.sRGB, red: 64/255, green: 64/255, blue: 64/255, opacity: 0.3),radius: 40, x:0,y:20)
+                        ForEach(self.imageListOfLibrary, id: \.self){image in
+                            
+                            Image(uiImage: image)
+                                .resizable()
+                                .cornerRadius(10)
+                                .scaledToFit()
+                                .frame(width: 85)
+                        }
                         ForEach(self.model.getPhotoList()){photo in
                             Image(uiImage: photo.image!)
                                 .resizable()
                                 .cornerRadius(10)
                                 .scaledToFit()
-                                .frame(width: 175)
-                            
+                                .frame(width: 85)
+
                         }.padding(.horizontal)
                             .shadow(color:Color(.sRGB, red: 64/255, green: 64/255, blue: 64/255, opacity: 0.3),radius: 40, x:0,y:20)
                         
                     }.padding()
                 }
-                //Image(uiImage: model.photo.image!)
-//                Button(action: {
-//                    print(self.model.getPhotoList())
-//                }, label: {
-//                    Text("click")
-//                })
-//                Image(systemName: "plus.viewfinder")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(width: 50, height: 50)
-//                    .shadow(radius: 10)
-//                    .onTapGesture { self.shouldPresentActionScheet = true }
-//                    .sheet(isPresented: $shouldPresentImagePicker) {
-//                        UmemImagePickerView(sourceType: self.shouldPresentCamera ? .camera : .photoLibrary, image: self.$image, isPresented: self.$shouldPresentImagePicker)
-//                            .environmentObject(model)
-//                }.actionSheet(isPresented: $shouldPresentActionScheet) { () -> ActionSheet in
-//                    ActionSheet(title: Text("Choose mode"), message: Text("Please choose one mode to add your memory photos"), buttons: [ActionSheet.Button.default(Text("Use Camera!"), action: {
-//                        self.shouldPresentImagePicker = true
-//                        self.shouldPresentCamera = true
-//                    }), ActionSheet.Button.default(Text("My Photo Library"), action: {
-//                        self.shouldPresentImagePicker = true
-//                        self.shouldPresentCamera = false
-//                    }), ActionSheet.Button.cancel()])
-//                }
+                .sheet(isPresented: $isShowPhotoLibray) {
+                    ImagePicker(sourceType: self.capturingType, selectedImageList: self.$imageListOfLibrary)
+                        .environmentObject(model)
+                }
+        .actionSheet(isPresented: $isShowChoosingSheet) { () -> ActionSheet in
+            ActionSheet(title: Text("Choose mode"), message: Text("Please choose one mode to add your memory photos"), buttons: [ActionSheet.Button.default(Text("Use Camera!"), action: {
+                self.isShowChoosingSheet = false
+                self.isShowPhotoLibray = true
+                self.capturingType = UIImagePickerController.SourceType.camera
+                
+            }), ActionSheet.Button.default(Text("My Photo Library"), action: {
+                
+                self.isShowChoosingSheet = false
+                self.isShowPhotoLibray = true
+                self.capturingType = UIImagePickerController.SourceType.photoLibrary
+                
+            }), ActionSheet.Button.cancel()])
+        }
             }
                        
-                       
-            }
                        
                        
     }
+}
     
     
 
