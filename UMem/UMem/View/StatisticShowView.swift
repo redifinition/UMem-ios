cfb45d90a453b3ae31c14199b0df3251d372d913
Qiao Liang <57929780+redifinition@users.noreply.github.com>
2021-12-26 01:36:45 +0800

    导入图表依赖

--- UMem/UMem/View/StatisticShowView.swift
+++ UMem/UMem/View/StatisticShowView.swift
@@ -7,9 +7,10 @@
 
 import SwiftUI
 
+
 struct StatisticShowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        MultiLineChartView(data: [([8,32,11,23,40,28], GradientColors.green), ([90,99,78,111,70,60,77], GradientColors.purple), ([34,56,72,38,43,100,50], GradientColors.orngPink)], title: "Title")
     }
 }
 
